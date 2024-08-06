73f59b142405cc78d981ab1ca3d89385ab559852
Matheus Cavalcante <matheuscavalcs@gmail.com>
2024-08-06 19:35:33 -0300

    add filament resources

---
+++ app/Filament/Resources/ComboItemResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ComboItemResource\Pages;
+use App\Filament\Resources\ComboItemResource\RelationManagers;
+use App\Models\ComboItem;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ComboItemResource extends Resource
+{
+    protected static ?string $model = ComboItem::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListComboItems::route('/'),
+            'create' => Pages\CreateComboItem::route('/create'),
+            'view' => Pages\ViewComboItem::route('/{record}'),
+            'edit' => Pages\EditComboItem::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ComboItemResource/Pages/ViewComboItem.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ComboItemResource\Pages;
+
+use App\Filament\Resources\ComboItemResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewComboItem extends ViewRecord
+{
+    protected static string $resource = ComboItemResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
