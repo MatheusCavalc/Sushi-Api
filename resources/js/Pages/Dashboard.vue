a400e683a4346e0385342cec72102178b05be9ef
Matheus Cavalcante <matheuscavalcs@gmail.com>
2024-08-07 12:40:49 -0300

    new logged message

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,7 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 flex justify-between items-center">
-                    <p class="text-gray-900">You're logged in!</p>
+                    <p class="text-gray-900">You're logged in! Voce esta logado!</p>
                     <a href="/admin">
                         <PrimaryButton>Admin</PrimaryButton>
                     </a>
