13cae523a467f93d1e85e453e61224c6064d8498
Matheus Cavalcante <matheuscavalcs@gmail.com>
2024-08-06 21:19:48 -0300

    admin link on dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
@@ -14,7 +15,12 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 flex justify-between items-center">
+                    <p class="text-gray-900">You're logged in!</p>
+                    <a href="/admin">
+                        <PrimaryButton>Admin</PrimaryButton>
+                    </a>
+                </div>
                 </div>
             </div>
         </div>
